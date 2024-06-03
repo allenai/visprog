5a54c36024cd57f56f68b99d05c75b1c77663e4b
Junital <2974033534@qq.com>
2024-06-03 18:47:59 +0800

    remove maskrcnn

--- maskrcnn_benchmark/data/datasets/object365.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torchvision
-import torch.utils.data as data
-from maskrcnn_benchmark.data.datasets.coco_dt import CocoDetectionTSV
-
-
-class Object365DetectionTSV(CocoDetectionTSV):
-    pass
