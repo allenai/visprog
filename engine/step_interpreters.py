a7ae1ccbeb97dd5f6857dc98f447fbe6618ce89c
Junital <2974033534@qq.com>
2024-06-03 18:46:56 +0800

    glip model

--- engine/step_interpreters.py
+++ engine/step_interpreters.py
@@ -1,6 +1,9 @@
 import cv2
 import os
 import torch
+import sys
+import timeit
+from typing import Union
 from openai import OpenAI
 import functools
 import numpy as np
@@ -17,6 +20,8 @@ from transformers import (ViltProcessor, ViltForQuestionAnswering,
 from diffusers import StableDiffusionInpaintPipeline
 from .object_graph import ObjectGraph, ObjectGraphGroup
 import re
+from maskrcnn_benchmark.engine.predictor_glip import GLIPDemo, to_image_list, create_positive_map, \
+            create_positive_map_label_to_token_from_positive_map
 
 from .nms import nms
 from vis_utils import html_embed_image, html_colored_span, vis_masks
@@ -395,6 +400,263 @@ Please follow this format to create the questions.
         prog_step.state[output_var] = merged_graph
 
         return merged_graph
+    
+class HiddenPrints:
+    hide_prints = False
+
+    def __init__(self, model_name=None, console=None, use_newline=True):
+        self.model_name = model_name
+        self.console = console
+        self.use_newline = use_newline
+        self.tqdm_aux = None
+
+    def __enter__(self):
+        if self.hide_prints:
+            import tqdm  # We need to do an extra step to hide tqdm outputs. Does not work in Jupyter Notebooks.
+
+            def nop(it, *a, **k):
+                return it
+
+            self.tqdm_aux = tqdm.tqdm
+            tqdm.tqdm = nop
+
+            if self.model_name is not None:
+                self.console.print(f'Loading {self.model_name}...')
+            self._original_stdout = sys.stdout
+            self._original_stderr = sys.stderr
+            sys.stdout = open(os.devnull, 'w')
+            # May not be what we always want, but some annoying warnings end up to stderr
+            sys.stderr = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.hide_prints:
+            sys.stdout.close()
+            sys.stdout = self._original_stdout
+            sys.stdout = self._original_stderr
+            if self.model_name is not None:
+                self.console.print(f'{self.model_name} loaded ')
+            import tqdm
+            tqdm.tqdm = self.tqdm_aux
+
+class GLIPLocInterpreter(GLIPDemo):
+    step_name = 'LOC'
+
+    def __init__(self, *args_demo):
+
+        working_dir = f'./pretrained_models/GLIP/'
+        
+        config_file = working_dir + "configs/glip_Swin_L.yaml"
+        weight_file = working_dir + "checkpoints/glip_large_model.pth"
+
+        kwargs = {
+            'min_image_size': 800,
+            'confidence_threshold': 0.5,
+            'show_mask_heatmaps': False
+        }
+
+        self.dev = "cuda" if torch.cuda.is_available() else "cpu"
+
+        from maskrcnn_benchmark.config import cfg
+
+        # manual override some options
+        cfg.local_rank = 0
+        cfg.num_gpus = 1
+        cfg.merge_from_file(config_file)
+        cfg.merge_from_list(["MODEL.WEIGHT", weight_file])
+        cfg.merge_from_list(["MODEL.DEVICE", self.dev])
+
+        with HiddenPrints("GLIP"), torch.cuda.device(self.dev):
+            from transformers.utils import logging
+            logging.set_verbosity_error()
+            GLIPDemo.__init__(self, cfg, *args_demo, **kwargs)
+        if self.cfg.MODEL.RPN_ARCHITECTURE == "VLDYHEAD":
+            plus = 1
+        else:
+            plus = 0
+        self.plus = plus
+        self.color = 255
+
+    @torch.no_grad()
+    def compute_prediction(self, original_image, original_caption, custom_entity=None):
+        image = self.transforms(original_image)
+        # image = [image, image.permute(0, 2, 1)]
+        image_list = to_image_list(image, self.cfg.DATALOADER.SIZE_DIVISIBILITY)
+        image_list = image_list.to(self.dev)
+        # caption
+        if isinstance(original_caption, list):
+
+            if len(original_caption) > 40:
+                all_predictions = None
+                for loop_num, i in enumerate(range(0, len(original_caption), 40)):
+                    list_step = original_caption[i:i + 40]
+                    prediction_step = self.compute_prediction(original_image, list_step, custom_entity=None)
+                    if all_predictions is None:
+                        all_predictions = prediction_step
+                    else:
+                        # Aggregate predictions
+                        all_predictions.bbox = torch.cat((all_predictions.bbox, prediction_step.bbox), dim=0)
+                        for k in all_predictions.extra_fields:
+                            all_predictions.extra_fields[k] = \
+                                torch.cat((all_predictions.extra_fields[k],
+                                            prediction_step.extra_fields[k] + loop_num), dim=0)
+                return all_predictions
+
+            # we directly provided a list of category names
+            caption_string = ""
+            tokens_positive = []
+            seperation_tokens = " . "
+            for word in original_caption:
+                tokens_positive.append([len(caption_string), len(caption_string) + len(word)])
+                caption_string += word
+                caption_string += seperation_tokens
+
+            tokenized = self.tokenizer([caption_string], return_tensors="pt")
+            # tokens_positive = [tokens_positive]  # This was wrong
+            tokens_positive = [[v] for v in tokens_positive]
+
+            original_caption = caption_string
+            # print(tokens_positive)
+        else:
+            tokenized = self.tokenizer([original_caption], return_tensors="pt")
+            if custom_entity is None:
+                tokens_positive = self.run_ner(original_caption)
+            # print(tokens_positive)
+        # process positive map
+        positive_map = create_positive_map(tokenized, tokens_positive)
+
+        positive_map_label_to_token = create_positive_map_label_to_token_from_positive_map(positive_map,
+                                                                                            plus=self.plus)
+        self.positive_map_label_to_token = positive_map_label_to_token
+        tic = timeit.time.perf_counter()
+
+        # compute predictions
+        with HiddenPrints():  # Hide some deprecated notices
+            predictions = self.model(image_list, captions=[original_caption],
+                                        positive_map=positive_map_label_to_token)
+        predictions = [o.to(self.cpu_device) for o in predictions]
+        # print("inference time per image: {}".format(timeit.time.perf_counter() - tic))
+
+        # always single image is passed at a time
+        prediction = predictions[0]
+
+        # reshape prediction (a BoxList) into the original image size
+        height, width = original_image.shape[-2:]
+        # if self.tensor_inputs:
+        # else:
+        #     height, width = original_image.shape[:-1]
+        prediction = prediction.resize((width, height))
+
+        if prediction.has_field("mask"):
+            # if we have masks, paste the masks in the right position
+            # in the image, as defined by the bounding boxes
+            masks = prediction.get_field("mask")
+            # always single image is passed at a time
+            masks = self.masker([masks], [prediction])[0]
+            prediction.add_field("mask", masks)
+
+        return prediction
+
+    @staticmethod
+    def to_left_right_upper_lower(bboxes):
+        return [(bbox[1], bbox[3], bbox[0], bbox[2]) for bbox in bboxes]
+
+    @staticmethod
+    def to_xmin_ymin_xmax_ymax(bboxes):
+        # invert the previous method
+        return [(bbox[2], bbox[0], bbox[3], bbox[1]) for bbox in bboxes]
+
+    @staticmethod
+    def prepare_image(image):
+        image = image[[2, 1, 0]]  # convert to bgr for opencv-format for glip
+        return image
+
+    @torch.no_grad()
+    def forward(self, image: torch.Tensor, obj: Union[str, list], return_labels: bool = False,
+                confidence_threshold=None):
+
+        if confidence_threshold is not None:
+            original_confidence_threshold = self.confidence_threshold
+            self.confidence_threshold = confidence_threshold
+
+        # if isinstance(object, list):
+        #     object = ' . '.join(object) + ' .' # add separation tokens
+        image = self.prepare_image(image)
+
+        # Avoid the resizing creating a huge image in a pathological case
+        ratio = image.shape[1] / image.shape[2]
+        ratio = max(ratio, 1 / ratio)
+        original_min_image_size = self.min_image_size
+        if ratio > 10:
+            self.min_image_size = int(original_min_image_size * 10 / ratio)
+            self.transforms = self.build_transform()
+
+        with torch.cuda.device(self.dev):
+            inference_output = self.inference(image, obj)
+
+        bboxes = inference_output.bbox.cpu().numpy().astype(int)
+        # bboxes = self.to_left_right_upper_lower(bboxes)
+
+        if ratio > 10:
+            self.min_image_size = original_min_image_size
+            self.transforms = self.build_transform()
+
+        bboxes = torch.tensor(bboxes)
+
+        # Convert to [left, lower, right, upper] instead of [left, upper, right, lower]
+        height = image.shape[-2]
+        bboxes = torch.stack([bboxes[:, 0], height - bboxes[:, 3], bboxes[:, 2], height - bboxes[:, 1]], dim=1)
+
+        if confidence_threshold is not None:
+            self.confidence_threshold = original_confidence_threshold
+        if return_labels:
+            # subtract 1 because it's 1-indexed for some reason
+            return bboxes, inference_output.get_field("labels").cpu().numpy() - 1
+        return bboxes
+    
+    def box_image(self,img,boxes,highlight_best=True):
+        img1 = img.copy()
+        draw = ImageDraw.Draw(img1)
+        for i,box in enumerate(boxes):
+            if i==0 and highlight_best:
+                color = 'red'
+            else:
+                color = 'blue'
+
+            draw.rectangle(box,outline=color,width=5)
+
+        return img1
+    
+    def parse(self,prog_step):
+        parse_result = parse_step(prog_step.prog_str)
+        step_name = parse_result['step_name']
+        img_var = parse_result['args']['image']
+        obj_name = eval(parse_result['args']['object'])
+        output_var = parse_result['output_var']
+        assert(step_name==self.step_name)
+        return img_var,obj_name,output_var
+    
+    def execute(self,prog_step,inspect=False):
+        img_var,obj_name,output_var = self.parse(prog_step)
+        img = prog_step.state[img_var]
+        
+        bboxes = self.forward(img,obj_name)
+
+        box_img = self.box_image(img, bboxes)
+
+        objs = dict(
+            box = bboxes,
+            category = obj_name,
+            img = img
+        )
+
+        prog_step.state[output_var] = objs
+        prog_step.state[output_var+'_IMAGE'] = box_img
+        if inspect:
+            html_str = self.html(img, box_img, output_var, obj_name)
+            return objs, html_str
+
+        return objs
+
 class LocInterpreter():
     step_name = 'LOC'
 
@@ -1639,7 +1901,7 @@ def register_step_interpreters(dataset='nlvr'):
         )
     elif dataset=='graph':
         return dict(
-            LOC=LocInterpreter(),
+            LOC=GLIPLocInterpreter(),
             BUILD=BuildInterpreter(),
             ADD=ADDInterpreter(),
             MERGE=MERGEInterpreter(),
